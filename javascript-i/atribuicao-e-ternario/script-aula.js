096c358ed1fa5cf324098c68a03160cee76a7979
Pavanelli7 <vittor.ribeiro@sptech.school>
2023-06-26 11:48:35 -0300

    Tópico/atribuicao-e-ternarios

---
+++ javascript-i/atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,24 @@
+var numero = 20;
+var numero2 = 10;
+
+numero += numero2; // numero = numero + numero2
+console.log(numero);
+console.log(numero2);
+
+var x = 5;
+var y = 10;
+x += y; // x = x + y (15)
+x -= y; // x = x - y (-5)
+x *= y; // x = x * y (50)
+x /= y; // x = x / y (0.5)
+x %= y; // x = x % y (0)
+x **= y; // x = x ** y (9765625)
+
+
+var idade = 20;
+
+var podeBeber = (idade >= 18) ? 'Pode beber' : 'Não pode beber';
+
+console.log(podeBeber);
+
+var possui 
